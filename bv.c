0bac0719595d000092ddf73222f78f8295a7bba2
John Woodfin <john.woodfin@gmail.com>
2017-09-04 15:21:00 -0400

    add serialize/deserialize, other fixes

--- bv.c
+++ bv.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-bv_ret_t bv_create(uint32_t num_entries, bitvector* bv)
+bv_ret_t bv_create(bitvector* bv, uint32_t num_entries)
 {
 	if ( bv == NULL ) {
 		return BV_NULL_ARG;
@@ -20,6 +20,7 @@ bv_ret_t bv_create(uint32_t num_entries, bitvector* bv)
 	if ( bv->bits == NULL ) {
 		return BV_OUT_OF_MEM;
 	} else {
+		bv->sz_of_bits = bytes_to_alloc;
 		memset(bv->bits, 0, sizeof(uint8_t));
 	}
 	bv->num_entries = num_entries;
@@ -29,6 +30,8 @@ bv_ret_t bv_create(uint32_t num_entries, bitvector* bv)
 bv_ret_t bv_destroy(bitvector* bv)
 {
 	free(bv->bits);
+	bv->num_entries = 0;
+	bv->sz_of_bits = 0;
 	bv->bits = NULL;
 	return BV_OK;
 }
@@ -50,7 +53,7 @@ int bv_get_bit(bitvector* bv, uint32_t bit_index)
 	return ret;
 }
 
-bv_ret_t bv_set_bit(bitvector* bv, uint32_t bit_index)
+static bv_ret_t bv_set_bit_to_val(bitvector* bv, uint32_t bit_index, uint32_t val)
 {
 	if ( bv == NULL ) {
 		return BV_NULL_ARG;
@@ -58,21 +61,95 @@ bv_ret_t bv_set_bit(bitvector* bv, uint32_t bit_index)
 		return BV_NOT_INIT;
 	} else if ( bit_index >= bv->num_entries ) {
 		return BV_IDX_OUT_OF_BOUNDS;
+	} else if ( val != 0 && val != 1 ) {
+		return BV_BAD_ARG;
 	}
 	
 	uint8_t byte_to_examine = bv->bits[bit_index / 8];
 	//bit_index = bit_index % 8;
 	uint8_t mask = 1 << (7 - (bit_index % 8));
-	bv->bits[bit_index / 8] = (byte_to_examine | mask);
+
+	if ( val == false ) {
+		bv->bits[bit_index / 8] = (byte_to_examine & ~mask);
+	} else {
+		bv->bits[bit_index / 8] = (byte_to_examine | mask);
+	}
 	return BV_OK;
 }
 
-int bv_test()
+bv_ret_t bv_clear_bit(bitvector* bv, uint32_t bit_index)
+{
+	return bv_set_bit_to_val(bv, bit_index, 0);
+}
+
+bv_ret_t bv_set_bit(bitvector* bv, uint32_t bit_index)
+{
+	return bv_set_bit_to_val(bv, bit_index, 1);
+}
+
+bv_ret_t bv_clear(bitvector* bv)
+{
+	if ( bv->bits != NULL ) {
+		memset(bv->bits, 0, bv->sz_of_bits);
+	} else {
+		return BV_NULL_ARG;
+	}
+
+	return BV_OK;
+}
+
+bv_ret_t bv_serialize(bitvector* bv, uint8_t** serialized_bv, uint32_t* serialized_bv_sz)
+{
+	if ( !bv || !serialized_bv || !serialized_bv_sz ) {
+		return BV_NULL_ARG;
+	}
+	
+	uint32_t sz_to_alloc = sizeof(bitvector) + bv->sz_of_bits;
+	*serialized_bv = (uint8_t*)malloc(sz_to_alloc);
+	if ( !(*serialized_bv) ) {
+		return BV_OUT_OF_MEM;
+	}
+
+	//assume the serializer and deserializer are the same endianness for now
+	*((uint32_t*)(*serialized_bv)) = bv->num_entries;
+	*((uint32_t*)(*serialized_bv + 4)) = bv->sz_of_bits;
+	memcpy((*serialized_bv + 8), bv->bits, bv->sz_of_bits);
+
+	*serialized_bv_sz = 8 + bv->sz_of_bits;
+	return BV_OK;
+}
+
+bv_ret_t bv_deserialize(bitvector** bv, uint8_t* serialized_bv)
+{
+	bv_ret_t ret = BV_OK; 
+	if ( !serialized_bv || !bv ) {
+		return BV_NULL_ARG;
+	}
+
+	*bv = (bitvector*)malloc(sizeof(bitvector));
+	if ( *bv == NULL ) {
+		return BV_OUT_OF_MEM;
+	}
+
+	//assume the serializer and deserializer are the same endianness for now	
+	ret = bv_create(*bv, *((uint32_t*)serialized_bv));
+	if ( ret != BV_OK ) {
+		return ret;
+	}
+		
+	(*bv)->sz_of_bits = *((uint32_t*)(serialized_bv + 4));
+	(*bv)->bits = (uint8_t*)malloc((*bv)->sz_of_bits);
+	memcpy((*bv)->bits, serialized_bv + 8, (*bv)->sz_of_bits);
+
+	return BV_OK;
+}
+
+int bv_test(void)
 {
 	int i;
 	bitvector bv;
 	bv_ret_t ret;
-	ret = bv_create(9, &bv);
+	ret = bv_create(&bv, 9);
 	if ( ret != BV_OK ) {
 		return -1;
 	}
@@ -113,5 +190,41 @@ int bv_test()
 		}
 	}
 
+	bv_destroy(&bv);
+
+	//test serialize/deserialize
+	bv_create(&bv, 128);
+	for ( i = 0; i < 128; i++ ) {
+		if ( i % 2 == 0 ) {
+			bv_set_bit(&bv, i);
+		}
+	}
+
+	uint8_t* serialized_bv = NULL;
+	uint32_t sz; 
+	ret = bv_serialize(&bv, &serialized_bv, &sz);
+	if ( ret != BV_OK ) {
+		return -9;
+	}
+
+	bitvector* deserialized_bv = { 0 };
+	
+	ret = bv_deserialize(&deserialized_bv, serialized_bv);
+	if ( ret != BV_OK ) {
+		return -9;
+	}
+
+	if ( bv.num_entries != deserialized_bv->num_entries ) {
+		return -10;
+	}
+	
+	if ( bv.sz_of_bits != deserialized_bv->sz_of_bits ) {
+		return -11;
+	}
+	
+	if ( memcmp(bv.bits, deserialized_bv->bits, bv.sz_of_bits) ) {
+		return -12;
+	}
+	
 	return 0;
 }
--- bv.h
+++ bv.h
@@ -1,20 +1,26 @@
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifndef __BV_H__
 #define __BV_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef enum {
 	BV_OK = 0,
 	BV_NULL_ARG = -100,
+	BV_BAD_ARG, 
 	BV_EMPTY,
 	BV_NOT_INIT,
 	BV_IDX_OUT_OF_BOUNDS,
 	BV_OUT_OF_MEM
 } bv_ret_t;
 
-
 typedef struct _bitvector {
 	uint32_t num_entries; /**< number of bits in the vector */
+	uint32_t sz_of_bits; /**< size of bits array (the internal array) in bytes */
 	uint8_t* bits; /**< the array of bytes containing the bits of the vector */
 } bitvector;
 
@@ -27,7 +33,7 @@ typedef struct _bitvector {
  * @note the bitvector will be initialized to zero
  * @return 0 on success, non-zero on failure
  */
-bv_ret_t bv_create(uint32_t num_entries, bitvector* bv); 
+bv_ret_t bv_create(bitvector* bv, uint32_t num_entries); 
 
 /**
  * Destroy a bitvector
@@ -55,7 +61,49 @@ int bv_get_bit(bitvector* bv, uint32_t bit_index);
  */
 bv_ret_t bv_set_bit(bitvector* bv, uint32_t bit_index);
 
+/**
+ * Clear a given bit in the vector, 0 indexed
+ *
+ * @param bv the bitvector to modify
+ * @param bit_index to clear, zero indexed, so clearing bit_index 0 will clear the first bit in the vector
+ * @return 0 on success, non-zero on error (eg, bit_index larger than bitvector size)
+ */
+bv_ret_t bv_clear_bit(bitvector* bv, uint32_t bit_index);
+
+/**
+ * Clear all set bits in a bitvector
+ *
+ * @return the return status
+ */
+bv_ret_t bv_clear(bitvector* bv);
+
+/**
+ * Serialize a given bitvector into a newly allocated uint8_t array, caller takes 
+ * control of serialized_bv
+ *
+ * @param bv the bitvector to serialize
+ * @param serialized_bv pointer to the uint8_t* that will be allocated and populated with the serialized version of bv
+ * @param serialized_bv_sz the size of serialized_bv
+ * @return return status
+ * @note caller takes control of memory allocated for serialized_bv
+ */
+bv_ret_t bv_serialize(bitvector* bv, uint8_t** serialized_bv, uint32_t* serialized_bv_sz);
+
+/**
+ * Deserialize a given uint8_t array into a newly allocated bitvector
+ *
+ * @param bv the bitvector that will be allocated and populated with the data in serialized_bv
+ * @param serialized_bv the serialized bv that is to be deserialized
+ * @return return status
+ * @note the caller is responsible for the newly allocated bv. It should be destroyed via bv_destroy and then free'd/deleted. 
+ */
+bv_ret_t bv_deserialize(bitvector** bv, uint8_t* serialized_bv);
+
 //self-test
-int bv_test();
+int bv_test(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __BV_H__
